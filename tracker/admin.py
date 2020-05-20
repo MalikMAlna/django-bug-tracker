921fbb5a1a228360417bf487e822f9bc660c7ce5
Mike A <jimalik2525@gmail.com>
2020-05-20 15:28:03 -0400

    Setting up authentication system from previous work

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# Code Citation: https://www.youtube.com/
+# watch?v=XJU9vRARkGo&list=PLgCYzUzKIBE_dil025VAJnDjNZHHHR9mW&index=13
+
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'display_name', 'date_joined',
+                    'last_login', 'is_admin', 'is_staff')
+    search_fields = ('email', 'username', 'display_name', 'age')
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
